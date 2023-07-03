78b6dc02b7e78ba0a214f1d82076a0b293dd6d40
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-07-03 15:56:17 +0200

    adding an icon to someof the footer components

--- src/theme/Footer/LinkItem/index.js
+++ src/theme/Footer/LinkItem/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import isInternalUrl from '@docusaurus/isInternalUrl';
-import IconExternalLink from '@theme/Icon/ExternalLink';
-export default function FooterLinkItem({item}) {
-  const {to, href, label, prependBaseUrlToHref, ...props} = item;
+import styles from './link.module.css';
+import * as Icons from 'lucide-react';
+import clsx from 'clsx';
+export default function FooterLinkItem({ item }) {
+  const { to, href, label, prependBaseUrlToHref, logo, ...props } = item;
   const toUrl = useBaseUrl(to);
-  const normalizedHref = useBaseUrl(href, {forcePrependBaseUrl: true});
+  const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true });
+  let IconComponent;
+  if (logo && logo.label) {
+    IconComponent = Icons[logo.label]
+  }
+  console.log(item)
   return (
     <Link
-      className="footer__link-item"
+      className={clsx("footer__link-item", styles.link)}
+
       {...(href
         ? {
-            href: prependBaseUrlToHref ? normalizedHref : href,
-          }
+          href: prependBaseUrlToHref ? normalizedHref : href,
+        }
         : {
-            to: toUrl,
-          })}
+          to: toUrl,
+        })}
       {...props}>
       {label}
-      {href && !isInternalUrl(href) && <IconExternalLink />}
+      {IconComponent && <IconComponent color={logo.color ?? undefined} size={logo.size ?? undefined} />}
+
+
     </Link>
   );
 }
