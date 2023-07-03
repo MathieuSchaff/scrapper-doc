d4e4558410108a7f3b86f2a52142723a8faaae54
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-07-03 11:04:16 +0200

    creating contribute page

---
+++ src/pages/contribute.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "@theme/Layout"
+
+export default function Contribute() {
+  return <Layout title="contribute" description="You can contribute to this project">
+    <h1>Contribute</h1>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        fontSize: '20px',
+      }}>
+      <p>
+        Edit <code>pages/contribute.jsx</code> and save to reload.
+      </p>
+    </div>
+  </Layout>
+}
